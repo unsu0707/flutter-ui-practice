210e8ecaa4670c28aeaf205a8d0bc5445d177b68
unsu0707 <unsu0707@gmail.com>
2024-05-05 12:16:03 +0900

    make 'order' param optional with default 0 and add automated offset dy for CurrencyCard

--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -6,6 +6,7 @@ class CurrencyCard extends StatelessWidget {
   final String code;
   final IconData icon;
   final bool isInverted;
+  final double order;
 
   const CurrencyCard({
     super.key,
@@ -14,6 +15,7 @@ class CurrencyCard extends StatelessWidget {
     required this.code,
     required this.icon,
     required this.isInverted,
+    this.order = 0,
   });
 
   Color bgColor() {
@@ -26,66 +28,70 @@ class CurrencyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: bgColor(),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
+    final double offsetDy = -20 * order;
+    return Transform.translate(
+      offset: Offset(0, offsetDy),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: bgColor(),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: textColor(),
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: textColor(),
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: textColor().withOpacity(0.8),
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
+                scale: 2,
+                child: Transform.translate(
+                  offset: const Offset(0, 14),
+                  child: Icon(
+                    icon,
                     color: textColor(),
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
+                    size: 86,
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: textColor(),
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: textColor().withOpacity(0.8),
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
-              scale: 2,
-              child: Transform.translate(
-                offset: const Offset(0, 14),
-                child: Icon(
-                  icon,
-                  color: textColor(),
-                  size: 86,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
